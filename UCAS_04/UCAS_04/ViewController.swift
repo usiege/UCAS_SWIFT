0f08ee6fd973cc5bf4ccdafc94619588439c3338
usiege <jianin45@qq.com>
2018-06-03 09:38:38 +0800

    添加第4节课

---
+++ UCAS_04/UCAS_04/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UCAS_04
+//
+//  Created by charles on 2018/6/3.
+//  Copyright © 2018年 lizhonghuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
